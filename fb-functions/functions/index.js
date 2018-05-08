2eadb6f2829c2b130566c663f29e58ca4c522a20
Islam M Ezz <38529841+IslamEzz@users.noreply.github.com>
2018-05-08 16:33:19 +0200

    creating firebase functions

---
+++ fb-functions/functions/index.js
@@ -0,0 +1,29 @@
+//import { QuerySnapshot } from '@google-cloud/firestore';
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+admin.initializeApp(functions.config().firebase);
+var db = admin.firestore();
+
+
+//add new value and update
+exports.funset = functions.https.onRequest((req, res) => {
+ const newdata = admin.firestore()
+ var docRef = newdata.collection('Users').doc();
+ var setData = docRef.set(data);
+});
+
+//get all documents in json like this 
+ exports.funget = functions.https.onRequest((req, res) => {
+  const store = admin.firestore()
+  const output = {}
+  store.collection('Users').get().then((snapshot) => {
+      snapshot.forEach((doc) => {
+        //console.log(doc.id, '=>', doc.data());
+        output[doc.id]=doc.data()
+      });
+      return res.json(output); 
+    })
+    .catch((err) => {
+      console.log('Error getting documents', err);
+    });      
+ });
