3de477fbf26d743951968395b3f2cb41916b5d79
Ahmed Hamdy <eng.ahr@hotmail.com>
2018-05-09 16:27:18 +0200

    create bid

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Tour from './components/tour';
+import BidingMain from './components/BidingMain';
 //import LoginForm from './components/LoginForm';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Tour/>
+        {/* <Tour/> */}
+        <BidingMain/>
       </View>
     );
   }
---
+++ components/CreateBid.js
@@ -0,0 +1,8 @@
+import React ,{Component} from 'react';
+import { View, Text } from 'react-native';
+
+
+export default class CreateBid extends React.Component{
+    
+
+}
